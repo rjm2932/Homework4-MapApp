82168a631ec9cd21200bda931b7f9c378e901c51
rjm2932 <ryanmccluretx@gmail.com>
2015-03-05 20:52:06 -0600

    added google-http-java-client

--- app/src/main/java/com/mycompany/homework4_mapapp/MapsActivity.java
+++ app/src/main/java/com/mycompany/homework4_mapapp/MapsActivity.java
@@ -1,19 +1,84 @@
 package com.mycompany.homework4_mapapp;
 
+import android.gesture.Prediction;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-//import com.mycompany.homework4_mapapp.R;
-//import android.R;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.client.util.Key;
 import com.mycompany.homework4_mapapp.MapsActivity;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.MapView;
+import com.google.api.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity {
+    static final HttpTransport HTTP_TRANSPORT = AndroidHttp.newCompatibleTransport();
+    static final JsonFactory JSON_FACTORY = new JacksonFactory();
+    private static final String PLACES_API_KEY = "AIzaSyDmiTwUlmU7_N1hM8YoTBaTG4LMNNp6DVw";
+    private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/geocode/json?";
+    private static final String TYPE_AUTOCOMPLETE = "address=";
+    private static final String OUT_JSON = "";
 
+
+    private ArrayList<String> autocomplete(String input) throws IOException {
+        ArrayList<String> resultList = new ArrayList<String>();
+
+
+            HttpRequestFactory requestFactory = HTTP_TRANSPORT.createRequestFactory(new HttpRequestInitializer() {
+                        @Override
+                        public void initialize(HttpRequest request) {
+                            request.setParser(new JsonObjectParser(JSON_FACTORY));
+                        }
+              }
+            );
+
+            GenericUrl url = new GenericUrl(PLACES_API_BASE + TYPE_AUTOCOMPLETE);
+            url.put("input", input); //address
+            url.put("key", PLACES_API_KEY);
+
+
+            HttpRequest request = requestFactory.buildGetRequest(url);
+            HttpResponse httpResponse = request.execute();
+            PlacesResult directionsResult = httpResponse.parseAs(PlacesResult.class);
+
+            List<Prediction> predictions = directionsResult.predictions;
+            for (Prediction prediction : predictions) {
+                resultList.add(prediction.description);
+            }
+
+        return resultList;
+    }
+    public static class PlacesResult {
+
+        @Key("predictions")
+        public List<Prediction> predictions;
+
+    }
+
+    public static class Prediction {
+        @Key("description")
+        public String description;
+
+        @Key("id")
+        public String id;
+
+    }
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
@@ -28,6 +93,8 @@ public class MapsActivity extends FragmentActivity {
         super.onResume();
         setUpMapIfNeeded();
     }
+
+    ArrayList<String> result = autocomplete("now");
 
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
@@ -66,5 +133,5 @@ public class MapsActivity extends FragmentActivity {
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
-    
+
 }
